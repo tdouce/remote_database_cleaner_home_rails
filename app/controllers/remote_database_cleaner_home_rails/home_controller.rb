ff92e65327dbaa3454b039f26caeae2a7e557478
tdouce <travisdouce@gmail.com>
2014-04-03 09:46:34 -0400

    Require that RDCHR be enabled

--- app/controllers/remote_database_cleaner_home_rails/home_controller.rb
+++ app/controllers/remote_database_cleaner_home_rails/home_controller.rb
@@ -4,9 +4,13 @@ module RemoteDatabaseCleanerHomeRails
     skip_before_filter *RemoteDatabaseCleanerHomeRails.skip_before_filters
     
     def create 
-      DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.clean
-      render json: {response: 200}
+      if RemoteDatabaseCleanerHomeRails.enabled?
+        DatabaseCleaner.strategy = :truncation
+        DatabaseCleaner.clean
+        render json: {response: 200}
+      else
+        render json: { status: 403 }, status: 403
+      end
     end
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -14,6 +14,8 @@ describe RemoteDatabaseCleanerHomeRails::HomeController do
 
   routes { RemoteDatabaseCleanerHomeRails::Engine.routes }
 
+  before { RemoteDatabaseCleanerHomeRails.enable! }
+
   describe '#create' do
     it 'should clean database with strategy truncation' do
       expect(DatabaseCleaner).to receive(:strategy=).with(:truncation)
@@ -24,6 +26,19 @@ describe RemoteDatabaseCleanerHomeRails::HomeController do
     it 'should skip :authentication and :another_authentication methods defined in ApplicationController' do
       post :create, {}
       expect(response).to_not redirect_to("/401.html")
+    end
+
+    it 'should return status code 403 when RemoteDatabaseCleanerHomeRails is not enabled' do
+      RemoteDatabaseCleanerHomeRails.disable!
+      post :create, {}
+      expect(response.status).to eq(403)
+    end
+
+    it 'should return status code 200 when RemoteDatabaseCleanerHomeRails is enabled' do
+      controller.stub(:factory)
+      RemoteDatabaseCleanerHomeRails.enable!
+      post :create, {}
+      expect(response.status).to eq(200)
     end
   end
 end
